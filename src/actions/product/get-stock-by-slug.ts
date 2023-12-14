8f8302b2b1053dafe6db5005bc4566e8f1e83687
ralexale <ralexale@gmail.com>
2023-12-14 11:43:40 +0900

    fin de seccion 18

---
+++ src/actions/product/get-stock-by-slug.ts
@@ -0,0 +1,15 @@
+"use server";
+
+export const getStockBySlug = async (slug: string): Promise<number> => {
+    try {
+        const stock = await prisma?.product.findFirst({
+            where: { slug },
+            select: { inStock: true },
+        });
+
+        return stock?.inStock ?? 0;
+    } catch (error) {
+        console.log(error);
+        return 0;
+    }
+};
--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,91 +1,93 @@
 export const revalidate = 10080; // 7 dias
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 import { getProductBySlug } from "@/actions";
 import { notFound } from "next/navigation";
-import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
+import {
+    ProductMobileSlideshow,
+    ProductSlideshow,
+    QuantitySelector,
+    SizeSelector,
+    StockLabel,
+} from "@/components";
 import { titleFont } from "@/config/fonts";
 
-
-
 interface Props {
-  params: { slug: string };
+    params: { slug: string };
 }
 
-
-
-
+// generamos la metadata dinamica por el nombre del producto
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = params;
-  const product = await getProductBySlug(slug);
+    const slug = params.slug;
+    const product = await getProductBySlug(slug);
 
-
-  return {
-    title: product?.title,
-    description: `pagina del producto ${product?.title}`
-  }
+    return {
+        title: product?.title ?? "producto no encontrado",
+        description: product?.description ?? "",
+        openGraph: {
+            title: product?.title ?? "producto no encontrado",
+            description: product?.description ?? "",
+            // images:[] // https://misitioweb.com/products/image.png
+            images: [`/products/${product?.images[1]}`],
+        },
+    };
 }
-
 
 export default async function ProductPage({ params }: Props) {
+    const { slug } = params;
 
-  const { slug } = params;
+    const product = await getProductBySlug(slug);
 
-  const product = await getProductBySlug(slug)
+    if (!product) {
+        notFound();
+    }
 
-  if (!product) {
-    notFound();
-  };
+    return (
+        <div className="mt-5  mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+            {/* Slideshow */}
+            <div className="col-span-1 sm:col-span-2 ">
+                {/* desktop slideshow */}
+                <ProductSlideshow
+                    images={product.images}
+                    title={product.title}
+                    className="hidden md:block"
+                />
 
+                <ProductMobileSlideshow
+                    images={product.images}
+                    title={product.title}
+                    className="block md:hidden"
+                />
+            </div>
 
+            {/* Detalles  */}
 
-  return (
-    <div className="mt-5  mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+            <div className="col-span-1 px-5 ">
+                <div className="flex flex-col gap-2">
+                    <StockLabel slug={slug} />
+                    <h1
+                        className={`${titleFont.className} antialiased font-bold text-xl`}
+                    >
+                        {product.title}
+                    </h1>
 
+                    <p className="text-xl mb-5 font-bold">${product.price}</p>
+                </div>
+                {/* Selector de tallas */}
+                <SizeSelector
+                    availableSizes={product.sizes}
+                    selectedSize={product.sizes[0]}
+                />
 
-      {/* Slideshow */}
-      <div className="col-span-1 sm:col-span-2 ">
+                {/* Selector de cantidad */}
+                <QuantitySelector quantity={product.inStock} />
 
-        {/* desktop slideshow */}
-        <ProductSlideshow
-          images={product.images}
-          title={product.title}
-          className="hidden md:block"
-        />
+                {/* botón  */}
+                <button className="btn-primary my-5">Agregar al carrito</button>
 
-        <ProductMobileSlideshow
-          images={product.images}
-          title={product.title}
-          className="block md:hidden"
-        />
-      </div>
-
-      {/* Detalles  */}
-
-
-      <div className="col-span-1 px-5 ">
-
-        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
-
-        <p className="text-lg mb-5 ">{product.price}</p>
-
-        {/* Selector de tallas */}
-        <SizeSelector
-          availableSizes={product.sizes}
-          selectedSize={product.sizes[0]}
-        />
-
-
-        {/* Selector de cantidad */}
-        <QuantitySelector quantity={product.inStock} />
-
-        {/* botón  */}
-        <button className="btn-primary my-5">Agregar al carrito</button>
-
-        {/* Descripción */}
-        <h3 className="font-bold text-sm">Descripción</h3>
-        <p className="font-light">{product.description}</p>
-      </div>
-
-    </div>
-  );
+                {/* Descripción */}
+                <h3 className="font-bold text-sm">Descripción</h3>
+                <p className="font-light">{product.description}</p>
+            </div>
+        </div>
+    );
 }
