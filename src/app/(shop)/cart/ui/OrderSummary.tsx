0b7208704243020c8d278e32dd41c342a379eee1
ralexale <ralexale@gmail.com>
2023-12-20 09:42:56 +0900

    fin de seccion 19

--- src/app/(shop)/cart/ui/OrderSummary.tsx
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -1,37 +1,44 @@
 "use client";
+import { Spinner } from "@/components";
 import { useCartStore } from "@/store";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
+import { useState, useEffect } from "react";
+import { currencyFormat } from "../../../../utils/currencyFormat";
 
-export const CartSummary = () => {
-    const [totalPrice, setTotalPrice] = useState(0);
-    const prodcutsInCart = useCartStore((state) => state.cart);
-    const totalProducts = useCartStore((state) => state.getTotalItems());
+export const OrderSummary = () => {
+    const [loaded, setLoaded] = useState(false);
+
+    const { itemsInCart, subTotal, tax, total } = useCartStore((state) =>
+        state.getSummaryInformation()
+    );
 
     useEffect(() => {
-        const subTotal = prodcutsInCart.reduce(
-            (prev, current) => prev + current.price,
-            0
-        );
-        setTotalPrice(subTotal);
-    }, [prodcutsInCart]);
+        setLoaded(true);
+    }, []);
+
+    if (!loaded) return <Spinner />;
+
+    itemsInCart === 0 && redirect("/empty");
 
     return (
         <div className="bg-white rounded-xl  shadow-xl p-7 h-[300px]">
             <h2 className="text-2xl mb-2">Resumen de orden</h2>
             <div className="grid grid-cols-2 ">
                 <span>No. Productos</span>
-                <span className="text-right">{totalProducts} Artículos</span>
+                <span className="text-right">
+                    {currencyFormat(itemsInCart)} Artículos
+                </span>
 
                 <span>Subtotal</span>
-                <span className="text-right">$ {totalPrice}</span>
+                <span className="text-right">{currencyFormat(subTotal)}</span>
 
                 <span>Impuestos (15%)</span>
-                <span className="text-right">$ {totalPrice * 0.15}</span>
+                <span className="text-right">{currencyFormat(tax)}</span>
 
                 <span className="text-2xl mt-5 ">Total</span>
                 <span className="text-right text-2xl mt-5">
-                    $ {totalPrice * 1.15}
+                    {currencyFormat(total)}
                 </span>
             </div>
 
